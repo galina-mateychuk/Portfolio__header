999feb45e654a945773c6d54c567bc44e90bc429
Galina <halyna.mateychuk@gamail.com>
2020-05-23 12:25:53 +0200

    Section-portfolio_5

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 $(document).
     ready(
-        function () {
+        function () {   
 
             const mobileMenuToggle = document.querySelector('.toggle-menu');
             const mobMenu = document.querySelector('.header-top_nav');
@@ -15,17 +15,35 @@ $(document).
                 bodyElement.classList.toggle('noscroll');
             })
 
-            window.addEventListener('resize', function(){
+            window.addEventListener('resize', function () {
                 mobMenu.classList.remove('active');
                 mobileMenuToggle.classList.remove('active');
                 overlay.classList.remove('active');
                 bodyElement.classList.remove('noscroll');
             })
 
-        let containerEl = document.querySelector('#portfolio-works');
-        let mixer = mixitup(containerEl, {
+            let containerEl = document.querySelector('#portfolio-works');
+            let mixer = mixitup(containerEl, {
+            });
             
-        })
-        
+            const filterToggles = document.querySelectorAll('.portfolio-nav button');
+            const portfolioBigCards = document.querySelectorAll('.portfolio-works__item');
+            for (let i = 0; i < filterToggles.length; i++) {
+
+                filterToggles[i].addEventListener('click', function () {
+                    if (i == 0) {
+                        for (let j = 0; j < 2; j++) {
+                            portfolioBigCards[j].classList.add('.portfolio-works__item-big')
+                        }
+                    }
+                    else {
+                        for (let j = 0; j < 2; j++) {
+                            portfolioBigCards[j].classList.remove('.portfolio-works__item')
+                        }
+                    }
+                });
+
+            }
+
         }
     );
