0a7ee65bfd896493b99a630a929a49fd34443338
Galina <halyna.mateychuk@gamail.com>
2020-05-24 17:03:29 +0200

    Section-contacts_2

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,7 @@
 $(document).
     ready(
-        function () {   
-
+        function () {
+            // Появление/отключение бокового меню
             const mobileMenuToggle = document.querySelector('.toggle-menu');
             const mobMenu = document.querySelector('.header-top_nav');
 
@@ -14,18 +14,18 @@ $(document).
                 overlay.classList.toggle('active');
                 bodyElement.classList.toggle('noscroll');
             })
-
+            // Отключение бокового меню при ресайзе
             window.addEventListener('resize', function () {
                 mobMenu.classList.remove('active');
                 mobileMenuToggle.classList.remove('active');
                 overlay.classList.remove('active');
                 bodyElement.classList.remove('noscroll');
             })
-
+            // Выборка карточек
             let containerEl = document.querySelector('#portfolio-works');
             let mixer = mixitup(containerEl, {
             });
-            
+            // Приводим карточки к одному размеру
             const filterToggles = document.querySelectorAll('.portfolio-nav button');
             const portfolioBigCards = document.querySelectorAll('.portfolio-works__item');
             for (let i = 0; i < filterToggles.length; i++) {
@@ -44,6 +44,19 @@ $(document).
                 });
 
             }
+            // fake-placeholder
+            const formInputs = document.querySelectorAll('.form-field')
+            for (let item of formInputs) {
 
+                const inputPlaceholder = item.nextElementSibling;
+                item.addEventListener('click', function () {
+                    inputPlaceholder.classList.add('active');
+                });
+                item.addEventListener('blur', function () {
+                    if (this.value == '') {
+                        inputPlaceholder.classList.remove('active');
+                    }
+                })
+            }
         }
     );
